a04114ebe5f29e2a652647b6953d1607331e3f0c
hausito <106822688+hausito@users.noreply.github.com>
2024-07-05 17:07:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -274,6 +274,7 @@ app.post('/saveUser', async (req, res) => {
     }
 });
 
+// Endpoint to update tickets
 app.post('/updateTickets', async (req, res) => {
     const { username, tickets } = req.body;
 
@@ -290,8 +291,6 @@ app.post('/updateTickets', async (req, res) => {
 
         if (result.rows.length > 0) {
             res.status(200).json({ success: true, data: result.rows[0] });
-
-
         } else {
             res.status(404).json({ success: false, error: 'User not found' });
         }
@@ -300,7 +299,6 @@ app.post('/updateTickets', async (req, res) => {
         res.status(500).json({ success: false, error: err.message });
     }
 });
-
 
 cron.schedule('26 14 * * *', async () => {
     try {
