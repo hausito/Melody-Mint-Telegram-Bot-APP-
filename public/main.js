a76f7dfe83a286a6ef761abe0dbe5df09eae2cb6
hausito <106822688+hausito@users.noreply.github.com>
2024-07-05 18:56:40 +0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-   document.addEventListener('DOMContentLoaded', async () => {
+    document.addEventListener('DOMContentLoaded', async () => {
             if (window.Telegram && window.Telegram.WebApp) {
                 window.Telegram.WebApp.expand();
                 await checkTicketClaimStatus();
@@ -8,16 +8,16 @@
     async function checkTicketClaimStatus() {
     const userInfo = document.getElementById('userInfo').textContent;
     try {
-        const response = await fetch(/getUserData?username=${encodeURIComponent(userInfo)});
+        const response = await fetch(`/getUserData?username=${encodeURIComponent(userInfo)}`);
         const data = await response.json();
         console.log('User data received:', data);
 
         if (data.success) {
             const ticketsInfo = document.getElementById('ticketsInfo');
-            ticketsInfo.textContent = ${data.tickets};
+            ticketsInfo.textContent = `${data.tickets}`;
 
             const pointsInfo = document.getElementById('points');  // Update points display
-            pointsInfo.textContent = ${data.points};
+            pointsInfo.textContent = `Points: ${data.points}`;
 
             if (!data.has_claimed_tickets) {
                 document.getElementById('claimPopup').style.display = 'block';
@@ -32,38 +32,40 @@
     }
 }
 
-async function claimTickets() {
-    const userInfo = document.getElementById('userInfo').textContent;
-    try {
-        const response = await fetch('/claimTickets', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ username: userInfo })
-        });
-        const data = await response.json();
 
-        if (data.success) {
-            alert('Tickets claimed successfully!');
-            document.getElementById('ticketsInfo').textContent = ${data.tickets};
-            document.getElementById('claimPopup').style.display = 'none';
-        } else {
-            alert(data.message);
-            document.getElementById('claimPopup').style.display = 'none';
+        async function claimTickets() {
+            const userInfo = document.getElementById('userInfo').textContent;
+            try {
+                const response = await fetch('/claimTickets', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ username: userInfo })
+                });
+                const data = await response.json();
+
+                if (data.success) {
+                    alert('Tickets claimed successfully!');
+                    document.getElementById('ticketsInfo').textContent = `Tickets: ${data.tickets}`;
+                    document.getElementById('claimPopup').style.display = 'none';
+                } else {
+                    alert(data.message);
+                    document.getElementById('claimPopup').style.display = 'none';
+                }
+            } catch (error) {
+                console.error('Error claiming tickets:', error);
+                alert('Failed to claim tickets. Please try again later.');
+            }
         }
-    } catch (error) {
-        console.error('Error claiming tickets:', error);
-        alert('Failed to claim tickets. Please try again later.');
-    }
-}
+
 
         async function showReferralLink() {
             const userInfo = document.getElementById('userInfo').textContent;
             try {
-                const response = await fetch(/getReferralLink?username=${encodeURIComponent(userInfo)});
+                const response = await fetch(`/getReferralLink?username=${encodeURIComponent(userInfo)}`);
                 const data = await response.json();
 
                 if (data.success) {
-                    const referralLink = https://t.me/melodymint_bot?start=${data.authCode};
+                    const referralLink = `https://t.me/melodymint_bot?start=${data.authCode}`;
 
                     // Display modal with referral link
                     const modal = document.getElementById('myModal');
@@ -81,7 +83,7 @@ async function claimTickets() {
                     document.body.removeChild(dummyInput);
 
                     // Display number of friends invited
-                    friendsInvited.textContent = Friends invited: ${data.friendsInvited};
+                    friendsInvited.textContent = `Friends invited: ${data.friendsInvited}`;
                 } else {
                     console.error('Failed to fetch referral link:', data.error);
                 }
