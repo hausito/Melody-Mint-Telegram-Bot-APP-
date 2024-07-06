5dca7a40439ddcb7d5712abba8c513dbf0bf12e0
hausito <106822688+hausito@users.noreply.github.com>
2024-07-06 20:59:29 +0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    if (window.Telegram && window.Telegram.WebApp) {
-        window.Telegram.WebApp.expand();
+    try {
+        if (window.Telegram && window.Telegram.WebApp) {
+            window.Telegram.WebApp.expand();
+        }
+
         await checkTicketClaimStatus();
-    } else {
-        console.warn('Telegram WebApp not available.');
+    } catch (error) {
+        console.error('Error initializing app:', error);
     }
 });
 
@@ -18,7 +21,7 @@ async function checkTicketClaimStatus() {
             const ticketsInfo = document.getElementById('ticketsInfo');
             ticketsInfo.textContent = `${data.tickets}`;
 
-            const pointsInfo = document.getElementById('points');  // Update points display
+            const pointsInfo = document.getElementById('points');
             pointsInfo.textContent = `${data.points}`;
 
             const claimPopup = document.getElementById('claimPopup');
@@ -35,7 +38,6 @@ async function checkTicketClaimStatus() {
     }
 }
 
-
 async function claimTickets() {
     const userInfo = document.getElementById('userInfo').textContent;
     try {
@@ -50,13 +52,9 @@ async function claimTickets() {
             alert('Tickets claimed successfully!');
             document.getElementById('ticketsInfo').textContent = `${data.tickets}`;
             document.getElementById('claimPopup').style.display = 'none';
-            
-            // Trigger event to update tickets in game.js
-            const event = new CustomEvent('ticketsUpdated', { detail: { tickets: data.tickets } });
-            document.dispatchEvent(event);
+            document.dispatchEvent(new CustomEvent('ticketsUpdated', { detail: { tickets: data.tickets } }));
         } else {
             alert(data.message);
-            document.getElementById('claimPopup').style.display = 'none';
         }
     } catch (error) {
         console.error('Error claiming tickets:', error);
@@ -64,53 +62,48 @@ async function claimTickets() {
     }
 }
 
-        async function showReferralLink() {
-            const userInfo = document.getElementById('userInfo').textContent;
-            try {
-                const response = await fetch(`/getReferralLink?username=${encodeURIComponent(userInfo)}`);
-                const data = await response.json();
+async function showReferralLink() {
+    const userInfo = document.getElementById('userInfo').textContent;
+    try {
+        const response = await fetch(`/getReferralLink?username=${encodeURIComponent(userInfo)}`);
+        const data = await response.json();
 
-                if (data.success) {
-                    const referralLink = `https://t.me/melodymint_bot?start=${data.authCode}`;
-
-                    // Display modal with referral link
-                    const modal = document.getElementById('myModal');
-                    const modalContent = document.getElementById('modalContent');
-                    const friendsInvited = document.getElementById('friendsInvited');
-                    modal.style.display = 'block';
-                    modalContent.textContent = referralLink;
-
-                    // Automatically copy to clipboard for mobile
-                    const dummyInput = document.createElement('input');
-                    document.body.appendChild(dummyInput);
-                    dummyInput.value = referralLink;
-                    dummyInput.select();
-                    document.execCommand('copy');
-                    document.body.removeChild(dummyInput);
-
-                    // Display number of friends invited
-                    friendsInvited.textContent = `Friends invited: ${data.friendsInvited}`;
-                } else {
-                    console.error('Failed to fetch referral link:', data.error);
-                }
-            } catch (error) {
-                console.error('Error fetching referral link:', error);
-            }
-        }
-
-        function closeModal() {
+        if (data.success) {
+            const referralLink = `https://t.me/melodymint_bot?start=${data.authCode}`;
             const modal = document.getElementById('myModal');
-            modal.style.display = 'none';
-        }
+            const modalContent = document.getElementById('modalContent');
+            const friendsInvited = document.getElementById('friendsInvited');
+            modal.style.display = 'block';
+            modalContent.textContent = referralLink;
 
-        function copyToClipboard() {
-            const referralLink = document.getElementById('modalContent').textContent;
             const dummyInput = document.createElement('input');
             document.body.appendChild(dummyInput);
             dummyInput.value = referralLink;
             dummyInput.select();
             document.execCommand('copy');
             document.body.removeChild(dummyInput);
-            alert('Referral link copied to clipboard!');
+
+            friendsInvited.textContent = `Friends invited: ${data.friendsInvited}`;
+        } else {
+            console.error('Failed to fetch referral link:', data.error);
         }
- 
+    } catch (error) {
+        console.error('Error fetching referral link:', error);
+    }
+}
+
+function closeModal() {
+    const modal = document.getElementById('myModal');
+    modal.style.display = 'none';
+}
+
+function copyToClipboard() {
+    const referralLink = document.getElementById('modalContent').textContent;
+    const dummyInput = document.createElement('input');
+    document.body.appendChild(dummyInput);
+    dummyInput.value = referralLink;
+    dummyInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummyInput);
+    alert('Referral link copied to clipboard!');
+}
